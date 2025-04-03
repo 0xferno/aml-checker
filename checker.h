4950374a19efecb832bbe5a513046f71e3e5b50b
0xferno <taubmannafewaxig@gmail.com>
2025-04-03 20:54:52 +0300

    upd 1.2

--- checker.h
+++ checker.h
@@ -16,7 +16,7 @@ namespace inet
 {
     std::string read_url(std::string url);
     DWORD post_request(std::string url, std::string request, DWORD* code, std::string* response);
-    bool webstatus_check(const std::string& szAddress, DWORD* pdwWebStat);
+    bool webstatus_check(const std::string& sAddress, DWORD* pdwWebStat);
 }
 
 
